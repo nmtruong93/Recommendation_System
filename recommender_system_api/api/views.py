03e4fa43e90a3f20500aa97311e4d481372a0f3b
nmtruong <nmtruong@tee-coin.com>
2019-10-10 16:45:32 +0700

    test vendor_id

--- recommender_system_api/api/views.py
+++ recommender_system_api/api/views.py
@@ -31,7 +31,7 @@ def get_main_page_recommendations(request, user_id=7973, gender=2, new_user=Fals
 
 @api_view(['GET', 'POST'])
 @renderer_classes([JSONRenderer])
-def get_custom_page_recommendations(request, user_id=7973, gender=2, vendor_id=10131):
+def get_custom_page_recommendations(request, user_id=7973, gender=2, vendor_id=56507):
     """
 
     :param request:

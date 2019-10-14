0265da37c51862ae60a30e9b6e2892fea82f7dc0
Nguyen Tran Ho Thanh Son <welcome@Welcomes-MacBook-Pro.local>
2019-10-14 16:34:09 +0700

    deploy

--- config/wsgi_dev.py
+++ config/wsgi_dev.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+app_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+sys.path.append(os.path.join(app_path, 'recommender_system_api'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
